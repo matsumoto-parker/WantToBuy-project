cf1ec3b474bfeb7a7e85d296f32e12b28c14f16b
Matsumoto Miu <miu.matsumoto04@gmail.comgit config --global user.email miu.matsumoto04@gmail.com>
2020-05-07 17:07:49 +0900

    fix(App.scss):scssをインポート

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import Header from './components/Header/Header';
 import Footer from './components/Footer/Footer';
-import './App.css';
+import './App.scss';
 
 function App() {
   return (
